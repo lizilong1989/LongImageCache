35a231aa87979507a17c4162d7bd07a47e61778c
lizilong1989 <15131968@qq.com>
2017-10-18 16:05:50 +0800

    update imagecache

--- src/LongImageCache.m
+++ src/LongImageCache.m
@@ -12,11 +12,18 @@
 #import "NSString+LongMD5.h"
 #import "UIImage+LongGif.h"
 
+#import <pthread.h>
+
+#define kLongDefaultSize 32
+
 static LongImageCache *instance = nil;
 
 @interface LongImageCache()
 {
     NSMutableDictionary *_imageDic;
+    NSMutableArray *_imageArray;
+    
+    pthread_mutex_t _mutex;
 }
 
 @end
@@ -37,6 +44,8 @@ static LongImageCache *instance = nil;
     self = [super init];
     if (self) {
         _imageDic = [NSMutableDictionary dictionary];
+        _imageArray = [NSMutableArray array];
+        pthread_mutex_init(&_mutex, NULL);
     }
     return self;
 }
@@ -47,7 +56,7 @@ static LongImageCache *instance = nil;
 {
     if (aKey.length > 0 && aData.length > 0) {
         UIImage *image = [UIImage imageWithData:aData];
-        [_imageDic setObject:image forKey:[aKey md5String]];
+        [self _setImage:image key:aKey];
         
         [[LongCache sharedInstance] storeCacheWithData:aData
                                                 forKey:aKey
@@ -60,12 +69,12 @@ static LongImageCache *instance = nil;
     if (aKey.length == 0) {
         return nil;
     }
-    UIImage *image = [_imageDic objectForKey:[aKey md5String]];
+    UIImage *image = [self _getImageWithKey:aKey];
     if (image == nil) {
         NSData *cacheData = [[LongCache sharedInstance] getCacheWithKey:aKey];
         if (cacheData) {
             image = [UIImage imageWithData:cacheData];
-            [_imageDic setObject:image forKey:[aKey md5String]];
+            [self _setImage:image key:aKey];
         }
     }
     return image;
@@ -74,6 +83,36 @@ static LongImageCache *instance = nil;
 - (void)clearImageCache
 {
     [_imageDic removeAllObjects];
+}
+
+#pragma mark - private
+- (void)_setImage:(UIImage*)aImage key:(NSString*)aKey
+{
+    pthread_mutex_lock(&_mutex);
+    if ([_imageArray count] > kLongDefaultSize) {
+        NSString *key = [_imageArray lastObject];
+        [_imageDic removeObjectForKey:key];
+        [_imageArray removeLastObject];
+
+        [_imageDic setObject:aImage forKey:[aKey md5String]];
+        [_imageArray insertObject:[aKey md5String] atIndex:0];
+    } else {
+        if ([_imageDic objectForKey:[aKey md5String]]) {
+            [_imageArray removeObject:[aKey md5String]];
+        }
+
+        [_imageDic setObject:aImage forKey:[aKey md5String]];
+        [_imageArray insertObject:[aKey md5String] atIndex:0];
+    }
+    pthread_mutex_unlock(&_mutex);
+}
+
+- (UIImage*)_getImageWithKey:(NSString*)aKey
+{
+    pthread_mutex_lock(&_mutex);
+    UIImage *image = [_imageDic objectForKey:[aKey md5String]];
+    pthread_mutex_unlock(&_mutex);
+    return image;
 }
 
 @end
