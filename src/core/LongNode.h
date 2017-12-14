fc30eb5c99fd557b25b315896de45500683d53cb
lizilong1989 <15131968@qq.com>
2017-12-14 10:45:35 +0800

    commit code

---
+++ src/core/LongNode.h
@@ -0,0 +1,30 @@
+//
+//  LongNode.h
+//  Pods
+//
+//  Created by EaseMob on 2017/12/12.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LongNode : NSObject
+
+@property (nonatomic, copy) NSString *key;
+@property (nonatomic, strong) LongNode *pre;
+@property (nonatomic, strong) LongNode *next;
+
+@end
+
+
+@interface LongNodeList : NSObject
+
+@property (nonatomic, strong) LongNode *head;
+@property (nonatomic, strong) LongNode *tail;
+
+- (instancetype)initWithNode:(LongNode*)aNode;
+
+- (void)insertNode:(LongNode*)aNode;
+
+- (void)removeNodeWithKey:(NSString *)aKey
+
+@end
---
+++ src/core/LongNode.m
@@ -0,0 +1,62 @@
+//
+//  LongNode.m
+//  Pods
+//
+//  Created by EaseMob on 2017/12/12.
+//
+
+#import "LongNode.h"
+
+@implementation LongNode
+
+@end
+
+@interface LongNodeList()
+{
+    NSMutableDictionary *_nodeDic;
+}
+@end
+
+@implementation LongNodeList
+
+- (instancetype)initWithNode:(LongNode*)aNode
+{
+    self = [self init];
+    if (self) {
+        _nodeDic = [NSMutableDictionary dictionary];
+        [_nodeDic setObject:aNode forKey:aNode.key];
+        _head = aNode;
+        _tail = aNode;
+    }
+    return self;
+}
+
+- (void)insertNode:(LongNode *)aNode
+{
+    if (![_nodeDic objectForKey:aNode.key]) {
+        [_nodeDic setObject:aNode forKey:aNode.key];
+        _tail.next = aNode;
+        aNode.pre = _tail;
+        _tail = aNode;
+    }
+}
+
+- (void)removeNodeWithKey:(NSString *)aKey
+{
+    if ([_nodeDic objectForKey:aKey]) {
+        LongNode *node = [_nodeDic objectForKey:aKey];
+        if (!node.pre) {
+            _head = node.next;
+            node.next.pre = nil;
+        } else if (!node.next) {
+            _tail = node.pre;
+            node.pre.next = nil;
+        } else {
+            LongNode *temp = node.next;
+            node.pre = temp.next;
+            temp.pre = node.pre;
+        }
+    }
+}
+
+@end
